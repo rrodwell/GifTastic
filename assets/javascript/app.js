1d49beef3d4e293cf5e8b26fe6526f09b00ceac1
Ryan Rodwell <ryanrodwell@gmail.com>
2017-03-30 19:06:19 -0400

    Updating ajax call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   function display(){
 
     var gifTopic = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifTopic + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifTopic + "&api_key=dc6zaTOxFJmzC&limit=10";
     console.log("query "+queryURL);
     $.ajax({
       url: queryURL,
